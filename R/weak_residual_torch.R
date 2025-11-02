ebe7913a5d18393d324bdf89bd396911fe7a5aab
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-11-02 15:17:26 -0700

    feat: J_wnnl in torch

--- R/weak_residual_torch.R
+++ R/weak_residual_torch.R
@@ -51,9 +51,9 @@ V_torch <- torch::torch_tensor(V)
 Vp <- test_fun_matrices$V_prime
 Vp_torch <- torch::torch_tensor(Vp)
 
-u <- do.call(c, lapply(1:ncol(U), function(i) S(paste0("u", i))))
-p <- do.call(c, lapply(1:length(p0), function(i) S(paste0("p", i))))
-t <- S("t")
+u <- do.call(c, lapply(1:ncol(U), function(i) symengine::S(paste0("u", i))))
+p <- do.call(c, lapply(1:length(p0), function(i) symengine::S(paste0("p", i))))
+t <- symengine::S("t")
 
 f_expr <- f(u, p, t)
 
@@ -84,6 +84,255 @@ sig_torch <- torch::torch_tensor(sig)
 b <- -1 * as.vector(Vp %*% U)
 b_torch <- torch::torch_tensor(b)
 }
+
+build_H_wnll <- function(S, Jp_S, L, Jp_L, Hp_L, Jp_r, Hp_r, g, b, J) {
+  function(p) {
+
+    r <- g(p) - b
+
+    Jp_rp <- Jp_r(p)
+    Hp_rp <- Hp_r(p)
+
+    Lp <- L(p)        # L(p)
+    Jp_Lp <- Jp_L(p)  # ∇ₚL(p)
+    Hp_Lp <- Hp_L(p)  # ∇ₚ∇ₚL(p)
+
+    Sp <- S(p)
+    Jp_Sp <- Jp_S(p)
+
+    S_inv_solve <- function(x) {
+      tryCatch({
+        cholF <- chol(Sp)
+        return(solve(cholF, solve(t(cholF), x)))
+      }, error = function(e1) {
+        tryCatch({
+          qrF <- qr(Sp)
+          return(solve(qrF, x))
+        }, error = function(e2) {
+          diag_reg <- 1e-12 * diag(nrow(Sp))
+          return(solve(Sp + diag_reg, x))
+        })
+      })
+    }
+
+    S_inv_rp <- S_inv_solve(r)
+    H_wnn <- matrix(0, nrow = J, ncol = J)
+
+    for (j in seq_len(J)) {
+      Jp_rp_j <- Jp_rp[, j]
+      Jp_Sp_j <- Jp_Sp[, , j]
+      Jp_Lp_j <- Jp_Lp[, , j]
+
+      shar_ <- S_inv_solve(Jp_Sp_j) # S⁻¹∂ⱼS
+
+      for (i in j:J) {
+        # ∂ᵢS(p) (Jacobian information)
+        Jp_Sp_i <- Jp_Sp[, , i]
+        Jp_Lp_i <- Jp_Lp[, , i]  # ∂ᵢL(p)
+        Jp_rp_i <- Jp_rp[, i]    # ∂ᵢr(p)
+
+        term <- Jp_Sp_i %*% shar_ # ∂ᵢSS⁻¹∂ⱼS
+
+        # ∂ᵢ∂ⱼS(p)
+        Hp_Lp_ji <- Hp_Lp[, , j, i]
+        p1 <- Jp_Lp_j %*% t(Jp_Lp_i)
+        p2 <- Hp_Lp_ji %*% t(Lp)
+        Hp_Sp_ji <- p1 + t(p1) + p2 + t(p2)  # ∂ᵢ∂ⱼS(p)
+
+        Hp_rp_ji <- Hp_rp[, j, i]  # ∂ᵢ∂ⱼ r(p)
+
+        prt0 <- as.numeric(t(Hp_rp_ji) %*% S_inv_rp)
+        prt1 <- -1.0 * as.numeric(t(S_inv_solve(Jp_rp_j)) %*% (Jp_Sp_i %*% S_inv_rp))
+
+        inv_factor <- S_inv_solve(Jp_rp_i)
+        prt2 <- as.numeric(t(Jp_rp_j) %*% inv_factor)
+
+        prt3 <- -2.0 * as.numeric(t(inv_factor) %*% (Jp_Sp_j %*% S_inv_rp))
+        prt4 <- -1.0 * as.numeric(t(S_inv_rp) %*% (Hp_Sp_ji %*% S_inv_rp))
+        prt5 <- 2 * as.numeric(t(S_inv_rp) %*% (term %*% S_inv_rp))
+
+        logDetTerm <- -1.0 * sum(diag(S_inv_solve(term))) + sum(diag(S_inv_solve(Hp_Sp_ji)))
+
+        Hij <- 0.5 * (2 * (prt0 + prt1 + prt2) + prt3 + prt4 + prt5 + logDetTerm)
+
+        H_wnn[j, i] <- Hij
+
+        if (i != j) {
+          H_wnn[i, j] <- Hij
+        }
+      }
+    }
+    return(H_wnn)
+  }
+}
+
+# Not working quite yet
+build_H_wnll_torch <- function(S, Jp_S, L, Jp_L, Hp_L, Jp_r, Hp_r, g, b, J) {
+  function(p) {
+    r <- g(p) - b
+
+    Jp_rp <- Jp_r(p)
+    Hp_rp <- Hp_r(p)
+
+    Lp <- L(p)
+    Jp_Lp <- Jp_L(p)
+    Hp_Lp <- Hp_L(p)
+
+    Sp <- S(p)
+    Jp_Sp <- Jp_S(p)
+
+    cholF <- torch::torch_cholesky(Sp)
+
+    S_inv_rp <- torch::torch_cholesky_solve(r$unsqueeze(2), cholF)
+
+    S_inv_Jp_Sp <- torch::torch_cholesky_solve(Jp_Sp, cholF)
+
+    H_wnn <- torch::torch_zeros(c(J, J), dtype = Sp$dtype, device = Sp$device)
+
+    for (j in seq_len(J)) {
+      Jp_rp_j_vec <- Jp_rp[, j]
+      Jp_Sp_j <- Jp_Sp[, , j]
+      S_inv_Jp_Sp_j <- S_inv_Jp_Sp[, , j]
+      Jp_Lp_j <- Jp_Lp[, , j]
+
+      Jp_rp_j_col <- Jp_rp[, j]$reshape(c(-1, 1))
+      S_inv_Jp_rp_j_vec <- torch::torch_cholesky_solve(Jp_rp_j_col, cholF)$squeeze(2)
+
+      for (i in j:J) {
+        S_inv_Jp_Sp_i <- S_inv_Jp_Sp[, , i]
+        Jp_Sp_i <- Jp_Sp[, , i]
+        Jp_Lp_i <- Jp_Lp[, , i]
+
+        Jp_rp_i_vec <- Jp_rp[, i]
+
+        Jp_rp_i_col <- Jp_rp[, i]$reshape(c(-1, 1))
+        S_inv_Jp_rp_i_vec <- torch::torch_cholesky_solve(Jp_rp_i_col, cholF)$squeeze(2)
+
+        Hp_rp_ji <- Hp_rp[, j, i]
+
+        term <- torch::torch_matmul(Jp_Sp_i, S_inv_Jp_Sp_j)
+
+        Hp_Lp_ji <- Hp_Lp[, , j, i]
+        p1 <- torch::torch_matmul(Jp_Lp_j, Jp_Lp_i$t())
+        p2 <- torch::torch_matmul(Hp_Lp_ji, Lp$t())
+        Hp_Sp_ji <- p1 + p1$t() + p2 + p2$t()
+
+        S_inv_Hp_Sp_ji <- torch::torch_cholesky_solve(Hp_Sp_ji, cholF)
+        S_inv_term <- torch::torch_cholesky_solve(term, cholF)
+
+        prt0 <- torch::torch_dot(Hp_rp_ji, S_inv_rp$squeeze(2))
+
+        prt1 <- -1.0 * torch::torch_dot(
+          S_inv_Jp_rp_j_vec,
+          torch::torch_matmul(Jp_Sp_i, S_inv_rp)$squeeze(2)
+        )
+
+        prt2 <- torch::torch_dot(Jp_rp_j_vec, S_inv_Jp_rp_i_vec)
+
+        prt3 <- -2.0 * torch::torch_dot(
+          S_inv_Jp_rp_i_vec,
+          torch::torch_matmul(Jp_Sp_j, S_inv_rp)$squeeze(2)
+        )
+
+        prt4 <- -1.0 * torch::torch_dot(
+          S_inv_rp$squeeze(2),
+          torch::torch_matmul(Hp_Sp_ji, S_inv_rp)$squeeze(2)
+        )
+
+        prt5 <- 2.0 * torch::torch_dot(
+          S_inv_rp$squeeze(2),
+          torch::torch_matmul(term, S_inv_rp)$squeeze(2)
+        )
+
+        logDetTerm <- -1.0 * torch::torch_trace(S_inv_term) + torch::torch_trace(S_inv_Hp_Sp_ji)
+
+        Hij <- 0.5 * (2.0 * (prt0 + prt1 + prt2) + prt3 + prt4 + prt5 + logDetTerm)
+
+        H_wnn[j, i] <- Hij
+
+        if (i != j) {
+          H_wnn[i, j] <- Hij
+        }
+      }
+    }
+
+    return(H_wnn)
+  }
+}
+
+
+
+H_wnll <- build_H_wnll(S, J_S, L, Jp_L, Hp_L, Jp_r, Hp_r, g, b, J)
+H_wnll_torch <- build_H_wnll_torch(S_torch, J_S_torch, L_torch, Jp_L_torch, Hp_L_torch, Jp_r_torch, Hp_r_torch, g_torch, b_torch, J)
+
+H_wnllp <- H_wnll(p)
+H_wnll_torchp <- H_wnll_torch(p)
+
+all.equal(H_wnllp, as.array(H_wnll_torchp))
+
+
+build_J_wnll <- function(S, Jp_S, Jp_r, g, b, J){
+  function(p){
+    Sp <- S(p)
+    J_Sp <- Jp_S(p)
+    J_rp <- Jp_r(p)
+    r <- g(p) - b
+
+    cholF <- chol(Sp)
+    S_inv_rp <- solve(cholF, solve(t(cholF), r))
+
+    gradient <- numeric(J)
+
+    for(j in seq(J)){
+      J_S_j <- J_Sp[, , j]
+      J_r_j <- J_rp[, j]
+
+      tmp <- J_S_j %*% S_inv_rp
+
+      prt0 <- 2.0 * (J_r_j %*% S_inv_rp)[1,1]
+      prt1 <- -1.0 * (S_inv_rp %*% tmp)[1,1]
+
+      fact <- solve(cholF, solve(t(cholF), J_S_j))
+      logDetPart <- sum(diag(fact))
+
+      gradient[j] <- 0.5 * (prt0 + prt1 + logDetPart)
+    }
+
+    return(gradient)
+  }
+}
+
+build_J_wnll_torch <- function(S, Jp_S, Jp_r, g, b, J){
+  function(p){
+    Sp <- S(p)
+    J_Sp <- Jp_S(p)
+    J_rp <- Jp_r(p)
+    J_rp_t <- J_rp$t()
+    r <- g(p) - b
+    cholF <- torch::torch_cholesky(Sp)
+
+    # Batched across all parameters J
+    S_inv_rp <- torch::torch_cholesky_solve(r$unsqueeze(2), cholF)
+    tmp <- torch::torch_einsum("ijk,j->ik", list(J_Sp, S_inv_rp$squeeze(2)))$unsqueeze(2)
+    prt0 <- 2 * torch::torch_matmul(J_rp_t, S_inv_rp)$squeeze(2)
+    prt1 <- -1.0 * torch::torch_einsum("ij,ijk -> k", list(S_inv_rp, tmp))
+    fact <- torch::torch_cholesky_solve(J_Sp, cholF)
+    diags <- torch::torch_diagonal(fact, dim1=1, dim2=2)
+    logDetPart <- torch::torch_sum(diags, dim=2)
+
+    gradient <- 0.5 * (prt0 + prt1 + logDetPart)
+    return(gradient)
+  }
+}
+
+J_wnll <- build_J_wnll(S, J_S, Jp_r, g, b, J)
+J_wnll_torch <- build_J_wnll_torch(S_torch, J_S_torch, Jp_r_torch, g_torch, b_torch, J)
+
+p <- p_star
+J_wnllp <- J_wnll(p)
+J_wnll_torchp <- J_wnll_torch(p)
+
+all.equal(J_wnllp, as.array(J_wnll_torchp))
 
 build_wnll <- function(S, g, b, K, D){
   constant_term <- 0.5 * K * D * log(2 * pi)
