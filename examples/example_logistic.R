d3a26289bcc56b31698a059ef21de2ffca35e6e4
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-11-14 12:06:27 -0700

    work on wnll

--- examples/example_logistic.R
+++ examples/example_logistic.R
@@ -36,28 +36,40 @@ tt <- matrix(sol[, 1], ncol = 1)
 res <- solveWendy(f, p0, U, tt, method = "MLE", optimize = T)
 #sol_hat <- deSolve::ode(u0, t_eval, modelODE, res$phat)
 
-Vp <- as.matrix(res$V_prime)
-U_state <- U
-svd_result <- svd(Vp)
-k <- length(svd_result$d)
-u_k <- svd_result$u[, 1:k]
-d_k <- svd_result$d[1:k]
-v_k <- svd_result$v[, 1:k]
-V_prime_pinv <- v_k %*% diag(1/d_k) %*% t(u_k)
+finite_J <- calc_gradient(p_star, res$wnll)
+analytic_J <- res$J_wnll(p_star)
 
-mp1 <- nrow(U)
-J <- length(p0)
+cat(finite_J)
+cat(analytic_J)
 
-for(i in seq(1:1)){
-  p_mat <- matrix(rep(res$phat, mp1), ncol = mp1, nrow = J)
-  input <- rbind(p_mat, t(U_state), t(tt))
-  FU_ <- res$f(input)
-  U_state <- -1 * V_prime_pinv %*% (as.matrix(res$V) %*% FU_)
-}
+finite_H <- calc_hessian(p_star, res$wnll)
+analytic_H <- res$H_wnll(p_star)
 
-plot(U_state, cex = 0.5, col = "blue", ylim = c(-1,1.5))
-points(U, cex = 0.5)
-points(sol[, 2], cex = 0.5, col = "green")
+cat(finite_H)
+cat(analytic_H)
+
+# Vp <- as.matrix(res$V_prime)
+# U_state <- U
+# svd_result <- svd(Vp)
+# k <- length(svd_result$d)
+# u_k <- svd_result$u[, 1:k]
+# d_k <- svd_result$d[1:k]
+# v_k <- svd_result$v[, 1:k]
+# V_prime_pinv <- v_k %*% diag(1/d_k) %*% t(u_k)
+#
+# mp1 <- nrow(U)
+# J <- length(p0)
+#
+# for(i in seq(1:1)){
+#   p_mat <- matrix(rep(res$phat, mp1), ncol = mp1, nrow = J)
+#   input <- rbind(p_mat, t(U_state), t(tt))
+#   FU_ <- res$f(input)
+#   U_state <- -1 * V_prime_pinv %*% (as.matrix(res$V) %*% FU_)
+# }
+#
+# plot(U_state, cex = 0.5, col = "blue", ylim = c(-1,1.5))
+# points(U, cex = 0.5)
+# points(sol[, 2], cex = 0.5, col = "green")
 #points(U, cex = 0.5, col = "red")
 
  # {
